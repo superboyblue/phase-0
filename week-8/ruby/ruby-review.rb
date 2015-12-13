3ec636f1d5994d745c517374293c832874ae6cf4
unknown <superboyblue@gmail.com>
2015-12-13 18:10:22 -0500

    Add file for ruby review

---
+++ week-8/ruby/ruby-review.rb
@@ -0,0 +1,24 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+# Reflection
